6dc83eed2e5ff7a0a3911a789f6f9be3e0089ecc
Muhammad Sameer Ali <samm_ali@hotmail.com>
2020-02-03 22:49:35 +0500

    Added a function balanceOf and a mapping to store addresses and their balances

--- contracts/MyToken.sol
+++ contracts/MyToken.sol
@@ -1,11 +1,18 @@
-pragma solidity 0.5.11;
+pragma solidity 0.5.12;
 
 contract MyToken{
 //We will be defining the total supply of our token in variable supply
 uint supply;
+//This mapping will be responsible to hold  the balances of our accounts.
+mapping (address => uint) balances;
 //This totalSupply() will be returning the total supply of our token
 function totalSupply() public view returns (uint256)
 {
     return supply;
+}
+//balanceOf function will take an address that address will be used as key in mapping to return the balance
+function balanceOf(address _owner) public view returns (uint256 balance)
+{
+        return balances[_owner];
 }
 }
